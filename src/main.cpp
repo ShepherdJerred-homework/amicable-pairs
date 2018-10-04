098b4684e3a49695583de652bb49f8a78394196f
Jerred Shepherd <ShepherdJerred@gmail.com>
2018-10-04 01:36:36 -0500

    Init

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <omp.h>
+#include <vector>
+
+using std::vector;
+using std::cin;
+using std::cout;
+using std::cerr;
+using std::endl;
+using std::ratio;
+using std::chrono::time_point;
+using std::chrono::duration;
+using std::chrono::high_resolution_clock;
+using std::chrono::duration_cast;
+using std::chrono::nanoseconds;
+
+int main() {
+
+}
